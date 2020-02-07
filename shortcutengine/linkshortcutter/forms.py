1b2e39107a7aaa139d3979fe8759f983f86e4051
valid <valid.saley@gmail.com>
2020-02-07 19:38:57 +0300

    form

---
+++ shortcutengine/linkshortcutter/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+
+class URLForm(forms.Form):
+    url = forms.URLField(label='url')
+
--- shortcutengine/linkshortcutter/views.py
+++ shortcutengine/linkshortcutter/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .forms import URLForm
+
 
 # Create your views here.
+
+def index(request):
+    form = URLForm()
+    return render(request, 'linkshortcutter/index.html', context={'form': form})
