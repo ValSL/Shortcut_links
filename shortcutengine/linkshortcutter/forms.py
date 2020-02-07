7189e2315761a7d668b8b5982a6b3bdfae5040a3
valid <valid.saley@gmail.com>
2020-02-07 22:50:51 +0300

    form fix

--- shortcutengine/linkshortcutter/forms.py
+++ shortcutengine/linkshortcutter/forms.py
@@ -5,7 +5,7 @@ from django.core.exceptions import ValidationError
 
 class URLForm(forms.Form):
     url = forms.URLField(label='url')
-    code = forms.CharField(max_length=5, label='code', help_text='By default, the code is auto-generated.')
+    code = forms.CharField(max_length=5, label='code', help_text='By default, the code is auto-generated.', required=False)
 
     def clean_code(self):
         urls = CuttedUrl.objects.all()
