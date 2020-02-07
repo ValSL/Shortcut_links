1ea7a6aadb9952e92989cbab8efca6ffed7cec9f
valid <valid.saley@gmail.com>
2020-02-07 20:30:14 +0300

    model class

--- shortcutengine/linkshortcutter/models.py
+++ shortcutengine/linkshortcutter/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+import random
+import string
 
-# Create your models here.
+
+def code_gen():
+    elements = string.ascii_lowercase + string.digits
+    code = ''.join(random.choice(elements) for x in range(5))
+    return code
+
+
+class CuttedUrl(models.Model):
+    code = models.CharField(max_length=5, default=code_gen)
+    real_url = models.URLField()
+    created = models.DateTimeField(auto_now_add=True)
+    clicks = models.IntegerField()
